7037fb85de0a0e378e0591ebe4871a28cdfc17c5
tyin418 <tyin418@gmail.com>
2016-07-29 15:55:55 -0400

    Var Question Quiz

---
+++ app_quiz_July29_3PM.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+            var multipleChoice = [
+                {
+                    questionNumber: 1,
+                    question: "What does CSS stand for?",
+                    choices: ["Cascading Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Colorful Style Sheets"],
+                    rightAnswer: "Cascading Style Sheets"
+        },
+                {
+                    number: 2,
+                    question: "Which HTML attribute is used to define inline styles?",
+                    choices: ["styles", "style", "font", "class"],
+                    rightAnswer: "class"
+        },
+                {
+                    number: 3,
+                    question: "Which property is used to change the background color?",
+                    choices: ["fgcolor", "color", "text-color"],
+                    rightAnswer: "text-color"
+        },
+                {
+                    number: 4,
+                    question: "Which CSS property controls the text size?",
+                    choices: ["font-size", "font-style", "text-size", "text-style"],
+                    rightAnswer: "text-size"
+
+        }
+    ];
